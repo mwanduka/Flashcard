a6505fa2b6cfe4d65c401f9464a1ddf23a19fe52
Joy Vivika <joiemumo@gmail.com>
2018-09-19 17:15:19 +0300

    added text method to scripts

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+      $("button#javascript").click(function() {
+      $("p").text("Javascript is a language that enables interaction with the web.");
+    });
+    $("button#methods").click(function() {
+    $("p").text("The actions taken on an element,usually depicted next to a parenthesis.");
+  });
+  $("button#function").click(function() {
+  $("p").text("A function is a procedure taken on an element.");
+});
+$("button#jquery").click(function() {
+$("p").text("jQuery is a single javascript file with in-built codes to help programmers build a cleaner web.");
+});
+$("button#alert").click(function() {
+$("p").text("Alert is a function that brings a pop up box.");
+});
+$("button#attributes").click(function() {
+$("p").text("This is a method that is used to describe an element.");
+});
+
+});
